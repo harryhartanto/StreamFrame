a973c907eb0979e1c897d4cc327c20bf3727ee7a
Harry Hartanto <harry.hartanto05@gmail.com>
2016-11-01 19:25:43 +0800

    rename connection into function.php finishing update parent algorithm adding interface

--- taskLegacy.php
+++ taskLegacy.php
@@ -1,52 +1,73 @@
 <html>
 <head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<div id="createTaskForm">
-	  <table border = '0' id='createForm' name='createForm' >
-		<thead>
-		<tr>
-			<td>New Task Name:</td>
-			<td><input type="text" name="newTaskName" id="newTaskName"></td>
-			<td id="info"></td>
-		</tr>
-		<tr>
-			<td>Modify Task Name: </td>
-			<!--td><input type="text" name="modifyTask" id="modifyTask"></td-->
-			<td>
-				<select name="modifyTask" id="modifyTask" onchange="refreshParentID()">
-			</td>
-			<td id="infoModify"></td>
-		</tr>
-		<tr>
-			<td>Parent Task ID: </td>
-			<td>
-				<select name="parentTaskID" id="parentTaskID">
-			</td>
-		</tr>
-		<tr>
-			<td><input type="button" id="submitBtn" value="Submit"></td>
-			<td><input type="button" id="test" value="Test" ></td>
-		</tr>
-		</table>
+	<div id="taskForm">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				
+				<h3 class="panel-title"><span class="glyphicon glyphicon-file"></span>Task Form</b></h3>
+			</div>
+			<div class="panel-body" style="font-size:13px;font-weight:normal;text-align:justify;">						  
+			  <table border = '0' id='createForm' name='createForm' >
+				<thead>
+				<tr>
+					<td>New Task Name:</td>
+					<td><input class="form-control" type="text" name="newTaskName" id="newTaskName"></td>
+					<td id="info"></td>
+				</tr>
+				<tr>
+					<td>Modify Task Name: </td>
+					<td>
+						<select  class="form-control" name="modifyTask" id="modifyTask" onchange="refreshParentID()"></select>
+					</td>
+					<td id="infoModify"></td>
+				</tr>
+				<tr>
+					<td>Parent Task ID: </td>
+					<td>
+						<select class="form-control" name="parentTaskID" id="parentTaskID"></select>
+					</td>
+				</tr>
+				<tr>
+					<td><button class="btn btn-success" type="button" id="submitBtn" value="Submit">Submit</button></td>
+				</tr>
+				</table>
+			</div>
+		</div>
 	</div>
-	<div id="listOfTasks"><div ></div>
-		  <table border = '0' id='filterView' name='filterView' >
-			<thead>
-			<tr>
-				<td>Filter : 
-				<select name="filterStatus" id="filterStatus" onchange="filterStatus()">
-					<option value='99'>ALL</option>
-					<option value='0'>IN PROGRESS</option>
-					<option value='1'>DONE</option>
-					<option value='2'>COMPLETE</option>
-				</td>
-			</tr>
-			<tr>
-				<td id="listTaskView"></td>
-			</tr>
-		</table>
+	<div id="listTasks">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3 class="panel-title"><span class="glyphicon glyphicon-tasks"></span> List Of Tasks</b></h3>
+			</div>
+			<div id="listOfTasks" class="panel-body" style="font-size:13px;font-weight:normal;text-align:justify;">
+				  <form class="form-horizontal" id='filterView' name='filterView' >
+					<div class="form-group">								  
+					  <div class="col-sm-12">
+						<span class="glyphicon glyphicon-filter"></span>
+						<select class="form" name="filterStatus" id="filterStatus" onchange="filterStatus()">
+							<option value='99'>ALL</option>
+							<option value='0'>IN PROGRESS</option>
+							<option value='1'>DONE</option>
+							<option value='2'>COMPLETE</option>
+						</select>
+					  </div>
+					</div>
+					<div class="form-group">
+						<div class="col-sm-12">
+							<div id="listTaskView"></div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+		
 	</div>
 	
 </body>
@@ -79,11 +100,13 @@ $(document).ready(function()
 			$("#infoModify").fadeIn(0);
 			var newTaskName=$("#newTaskName").val();
 			var selectedParentTaskID = $("#parentTaskID").val();
-			if(!newTaskName.trim())	//insert action
+			if(!newTaskName.trim())				//insert action
 			{
 				var selectedModifyTaskID=$("#modifyTask").val();
 				var selectedModifyTaskName=$("#modifyTask option:selected").text();
-				$.post("connection.php",
+				if(selectedModifyTaskName!="Please Select")
+				{
+					$.post("function.php",
 					{
 						modifyTaskID: selectedModifyTaskID,
 						parentTaskID: selectedParentTaskID,
@@ -95,13 +118,18 @@ $(document).ready(function()
 						$("#infoModify").fadeOut(5000);
 						//alert(selectedModifyTaskName+" "+data);
 						refreshListOfTask();
-						// alert("Data: " + data + "\nStatus: " + status);
 					}
-				);
+					);
+				}
+				else
+				{
+					$("#infoModify").text("Please select one of the task to be modified.");
+					$("#infoModify").fadeOut(5000);
+				}
 			}
 			else 							   //modify action
 			{
-				$.post("connection.php",
+				$.post("function.php",
 					{
 						taskName: newTaskName,
 						parentTaskID: selectedParentTaskID,
@@ -114,51 +142,35 @@ $(document).ready(function()
 						$("#info").text(newTaskName+" "+data);
 						$("#info").fadeOut(5000);
 						refreshListOfTask();
-						// alert("Data: " + data + "\nStatus: " + status);
 					}
 				);
 			}
 		}
 	);
 
-	$("#test").click(
-		function()
-		{
-			$.post("connection.php",
-				{
-					action: 'SELECT_ALL'
-				},
-				function(data, status)
-				{
-					$("#parentTaskID").html(data);
-				}
-			);
-			// var result = "<option value='0'>Please Select</option><option value='1'>Yaha!</option>";
-			// $("#parentTaskID").html(result);
-		}
-	);
 
-	function refreshListOfTask()
-	{
-		$.post("connection.php",
-			{
-				action: 'SELECT_ALL',
-				taskID: 'ALL'
-			},
-			function(data, status)
-			{
-				$("#parentTaskID").html(data);
-				$("#modifyTask").html(data);
-			}
-		);		
-		
-		refreshListViewTask();
-	}
 });
+
+function refreshListOfTask()
+{
+	$.post("function.php",
+		{
+			action: 'SELECT_ALL',
+			taskID: 'ALL'
+		},
+		function(data, status)
+		{
+			$("#parentTaskID").html(data);
+			$("#modifyTask").html(data);
+		}
+	);		
+	
+	refreshListViewTask();
+}
 
 function refreshListViewTask()
 {
-	$.post("connection.php",
+	$.post("function.php",
 			{
 				action: 'SHOW_NESTED_LIST',
 				status: '99'			//ALL
@@ -172,7 +184,7 @@ function refreshListViewTask()
 function refreshParentID()
 {
 	var selectedModifyTask = $("#modifyTask").val();
-	$.post("connection.php",
+	$.post("function.php",
 		{
 			action: 'SELECT_ALL',
 			taskID: selectedModifyTask
@@ -187,7 +199,7 @@ function refreshParentID()
 function filterStatus()
 {
 	var selectedFilterStatus = $("#filterStatus").val();
-	$.post("connection.php",
+	$.post("function.php",
 		{
 			action: 'SHOW_FLAT_LIST',
 			status: selectedFilterStatus
@@ -201,7 +213,7 @@ function filterStatus()
 
 function updateStatus(selectedID, selectedStatus)
 {
-	$.post("connection.php",
+	$.post("function.php",
 		{
 			action: 'UPDATE_TASK_STATUS',
 			id: selectedID,
@@ -222,12 +234,34 @@ function changeStatus(checkBox) {
   var arrValue = temp.split("-");
   if(checkBox.checked==true)
   {
-	  updateStatus(arrValue[0],1); 		//update to DONE
+	  updateStatus(arrValue[0],2); 		//update to COMPLETED
   }
   else
   {
 	  updateStatus(arrValue[0],0);		//update to IN PROGRESS
   }
+}
+function changeTitle(header,e, selectedID)
+{
+	var newTaskName = header.innerHTML;
+	if(e.keyCode==13)
+	{
+		$.post("function.php",
+		{
+			action: 'UPDATE_TASK_NAME',
+			id: selectedID,
+			taskName: newTaskName
+		},
+		function(data, status)
+		{
+			if(status=="success")
+			{
+				refreshListOfTask();
+			}			
+		}
+		);
+		
+	}
 }
 </script>
 </html>
